e01bd35f28f9137a8a30c0daf5471425b19e6dc2
la26092002 <larbibenyakhou@gmail.com>
2024-07-24 18:36:51 +0100

    third commit

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv')
 const connectDB = require('./config/db');
 const cors = require('cors')
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 dotenv.config();
@@ -15,12 +20,21 @@ app.use(cors());
 // Body parser middleware
 app.use(express.json());
 
-//app.use(express.json({extended:false}))
+app.use(helmet());
+
+
+//rateLimit
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
 
 // Define Routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/Order', require('./routes/api/Order'));
-//app.use('/api/event', require('./routes/api/event'));
 app.use('/api/DataInserted', require('./routes/api/DataInserted'));
 
 const PORT = process.env.PORT || 5000;
